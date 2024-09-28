6d2b77bf2902d4eaa132651f321a5aaa4cc34ad4
Felix <92216380+Felix200260@users.noreply.github.com>
2024-09-28 16:53:43 +0500

    [~] Первый коммит

---
+++ studyEnglishWords/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace studyEnglishWords.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
