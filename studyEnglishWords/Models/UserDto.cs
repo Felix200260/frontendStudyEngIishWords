1f788538429b510364cba120d9f75ec414f620cf
Felix <92216380+Felix200260@users.noreply.github.com>
2024-12-23 08:23:11 +0500

    [~] Решил проблему с добавлением почты в БД

--- studyEnglishWords/Models/UserDto.cs
+++ studyEnglishWords/Models/UserDto.cs
@@ -1,11 +1,18 @@
 
+using System.Text.Json.Serialization;
+
 namespace studyEnglishWords.Models
 {
     public class UserDto
     {
         public int Id { get; set; }
-        public string? First_name { get; set; } 
+        [JsonPropertyName("first_name")]
+        public string? First_name { get; set; }
+
+        [JsonPropertyName("unique_email")]
         public string? UniqueEmail { get; set; }
+
+        [JsonPropertyName("password")]
         public string? Password { get; set; }
     }
 }
