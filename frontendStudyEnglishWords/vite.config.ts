7620a4fa12017c5cde0250d199a7f7eb96ecf063
Vali-86 <student@example.com>
2025-04-17 20:05:15 +0500

    [~] Настроил vite.config.ts

--- frontendStudyEnglishWords/vite.config.ts
+++ frontendStudyEnglishWords/vite.config.ts
@@ -1,18 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
     vueDevTools({
-      launchEditor: 'rider'
-    })
+      launchEditor:
+        'C:\\Program Files\\JetBrains\\WebStorm 2024.3.5\\bin\\webstorm64.exe'
+    }),
+    vueJsx()
   ],
   resolve: {
     alias: {
@@ -32,4 +33,4 @@ export default defineConfig({
   build: {
     outDir: '../wwwroot' // Путь для выходных файлов фронтенда
   }
-})
+});
